28034f5367724ce4887d50d7f522ffc80a5e2d1b
bsahd <193844131+bsahd@users.noreply.github.com>
2025-03-12 17:00:06 +0900

    pub to jsr

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
-// deno-lint-ignore-file no-inner-declarations
+// deno-lint-ignore-file no-inner-declarations no-var
+
 
 export interface CosenseConstructorOptions {
 	/**
@@ -13,10 +14,12 @@ export interface CosenseConstructorOptions {
 
 export class CosenseClient {
 	options: CosenseConstructorOptions;
-	constructor(options: CosenseConstructorOptions) {
+	projectName:string;
+	protected constructor(projectName:string,options: CosenseConstructorOptions) {
 		this.options = options;
+		this.projectName=projectName;
 	}
-	async fetch(url: string, options?: RequestInit) {
+	protected async fetch(url: string, options?: RequestInit): Promise<Response> {
 		return await fetch(
 			url,
 			this.options.sessionid
@@ -31,43 +34,54 @@ export class CosenseClient {
 	}
 }
 export class Project extends CosenseClient {
-	id: string;
+	id!: string;
 	name: string;
-	displayName: string;
-	publicVisible: boolean;
-	loginStrategies: string[];
-	plan: string;
-	theme: string;
-	gyazoTeamsName: string | null;
+	displayName!: string;
+	publicVisible!: boolean;
+	loginStrategies!: string[];
+	plan!: string;
+	theme!: string;
+	gyazoTeamsName!: string | null;
 	image?: string;
-	translation: boolean;
-	infobox: boolean;
-	created: number;
-	updated: number;
-	isMember: boolean;
-	constructor(
+	translation!: boolean;
+	infobox!: boolean;
+	created!: number;
+	updated!: number;
+	isMember!: boolean;
+	
+	static async new(
+		projectName: string,
+		options?: CosenseConstructorOptions,
+	): Promise<Project> {
+		var a = await fetch(
+			"https://scrapbox.io/api/projects/" +
+				encodeURIComponent(projectName),
+			options?.sessionid
+				? {
+					headers: { "Cookie:": "connect-sid=" + options.sessionid },
+				}
+				: {},
+		);
+		if (!a.ok) {
+			throw new Error(await a.text());
+		}
+		return new Project(
+			projectName,
+			options ? options : { allowediting: false, sessionid: null },
+			await a.json(),
+		);
+	}
+	
+	private constructor(
 		name: string,
 		options: CosenseConstructorOptions,
 		projectInfo: Project,
 	) {
-		super(options);
+		super(name,options);
 		this.name = name;
-		this.id = projectInfo.id;
-		this.name = projectInfo.name;
-		this.displayName = projectInfo.displayName;
-		this.publicVisible = projectInfo.publicVisible;
-		this.loginStrategies = projectInfo.loginStrategies;
-		this.plan = projectInfo.plan;
-		this.theme = projectInfo.theme;
-		this.gyazoTeamsName = projectInfo.gyazoTeamsName;
-		this.image = projectInfo.image;
-		this.translation = projectInfo.translation;
-		this.infobox = projectInfo.infobox;
-		this.created = projectInfo.created;
-		this.updated = projectInfo.updated;
-		this.isMember = projectInfo.isMember;
+		Object.assign(this, projectInfo);
 	}
-	async *pageList() {
+	async *pageList(): AsyncGenerator<PageListItem, void, unknown> {
 		var followId: string | null = null;
 		while (true) {
 			var res: PageListItem[] = await (await this.fetch(
@@ -81,71 +95,123 @@ export class Project extends CosenseClient {
 			}
 			followId = res[res.length - 1].id;
 			for (const e of res) {
-				yield new PageListItem(
-					e.id,
-					e.title,
-					e.links,
-					e.updated,
-					this.options,
-					this.name,
-				);
+				yield new PageListItem({
+					...e,
+					options: this.options,
+					project: this.name,
+				});
 			}
 		}
 	}
-}
-
-class PageListItem extends CosenseClient {
-	id: string;
-	title: string;
-	links: string[];
-	updated: number;
-	project: string;
-	constructor(
-		id: string,
-		title: string,
-		links: string[],
-		updated: number,
-		options: CosenseConstructorOptions,
-		project: string,
-	) {
-		super(options);
-		this.id = id;
-		this.title = title;
-		this.links = links;
-		this.updated = updated;
-		this.project = project;
-	}
-	async getDetail() {
-		return await (await this.fetch(
-			"https://scrapbox.io/api/pages/" +
-				this.project + "/" + encodeURIComponent(this.title),
-		)).json();
+	getPage(pageName:string){
+		return Page.new(this.projectName,pageName,this)
 	}
 }
 
-export async function cosenseFactory(
-	projectName: string,
-	options?: CosenseConstructorOptions,
-) {
-	var a = await fetch(
-		"https://scrapbox.io/api/projects/" +
-			encodeURIComponent(projectName),
-		options?.sessionid
-			? {
-				headers: { "Cookie:": "connect-sid=" + options.sessionid },
-			}
-			: {},
-	);
-	if (!a.ok) {
-		throw new Error(await a.text());
+export class PageListItem extends CosenseClient {
+	id!: string;
+	title!: string;
+	links!: string[];
+	updated!: number;
+
+	constructor(init: {
+		id: string;
+		title: string;
+		links: string[];
+		updated: number;
+		options: CosenseConstructorOptions;
+		project: string;
+	}) {
+		super(init.project,init.options);
+		Object.assign(this, init); 
 	}
-	return new Project(
-		projectName,
-		options ? options : { allowediting: false, sessionid: null },
-		await a.json(),
-	);
+
+	getDetail() {
+		return Page.new(this.projectName,this.title,this)
+	}
 }
-var vp = await cosenseFactory("villagepump");
+
+export class Page extends CosenseClient{
+	id!: string; 
+	title!: string; 
+	image!: string; 
+	descriptions!: string[]; 
+	pin!: 0 | 1; 
+	views!: number; 
+	linked!: number; 
+	commitId?: string; 
+	created!: number; 
+	updated!: number; 
+	accessed!: number; 
+	lastAccessed!: number | null; 
+	snapshotCreated!: number | null; 
+	snapshotCount!: number; 
+	pageRank!: number; 
+	persistent!: boolean; 
+	lines!: {
+		id: string; 
+		text: string; 
+		userId: string; 
+		created: number; 
+		updated: number;
+	}[];
+	links!: string[]; 
+	icons!: string[]; 
+	files!: string[]; 
+	relatedPages!: {
+		links1hop: {
+			id: string; 
+			title: string; 
+			titleLc: string;
+			image: string; 
+			descriptions: string[]; 
+			linksLc: string[];
+			linked: number; 
+			updated: number; 
+			accessed: number; 
+		}[];
+		links2hop: {
+			id: string; 
+			title: string; 
+			titleLc: string;
+			image: string; 
+			descriptions: string[]; 
+			linksLc: string[]; 
+			linked: number; 
+			updated: number; 
+			accessed: number; 
+		}[];
+		hasBackLinksOrIcons: boolean;
+	};
+	user!: {
+		id: string;
+		name: string;
+		displayName: string;
+		photo: string;
+	};
+	collaborators!: {
+		id: string;
+		name: string;
+		displayName: string;
+		photo: string;
+	}[];
+	static async new(projectName:string,pageName:string,options:CosenseClient){
+		return new Page(
+			await (await options.fetch(
+				`https://scrapbox.io/api/pages/${projectName}/${
+					encodeURIComponent(pageName)
+				}`,
+			)).json(),projectName,options
+		);
+	}
+	private constructor(init: Page,projectName:string,options:CosenseClient) {
+		super(projectName,options.options)
+		Object.assign(this, init);
+	}
+}
+
+var vp = await Project.new("villagepump");
 for await (const element of vp.pageList()) {
 	console.log(element);
 }
+console.log(await vp.getPage("井戸端"))
